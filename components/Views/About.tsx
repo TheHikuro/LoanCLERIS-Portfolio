4ef253d52565f95cb7e6e7aaade5809b29bc41a1
Loan CLERIS <loan.cleris@gmail.com>
2025-12-11 21:06:21 +0100

    chore(deps): update deps and fix Link from Next

--- components/Views/About.tsx
+++ components/Views/About.tsx
@@ -6,45 +6,46 @@ import { Title } from '../index'
 import { useTranslation } from 'react-i18next'
 
 export const About = () => {
-    const { t } = useTranslation()
-    return (
-        <motion.div
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
-            transition={{ duration: 1.5 }}
-            className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
-        >
-            <Title title={t('About')} />
-            <motion.div
-                initial={{ x: -200, opacity: 0 }}
-                transition={{ duration: 1.2 }}
-                whileInView={{ x: 0, opacity: 1 }}
-                viewport={{ once: true }}
-                className="about-image-container mt-36 md:mt-0"
-            >
-                <Image
-                    src={loan}
-                    className="about-image"
-                    objectFit='cover'
-                    layout='responsive'
-                />
-            </motion.div>
-            <motion.div
-                initial={{ x: 200, opacity: 0 }}
-                transition={{ duration: 1.2 }}
-                whileInView={{ x: 0, opacity: 1 }}
-                viewport={{ once: true }}
-                className='space-y-5 px-0 md:px-10 mt-40 md:mt-0'
-            >
-                <h4 className='text-3xl md:text-4xl font-semibold'>
-                    {t('AboutPage_text_1')}{" "}
-                    <span className='underline decoration-[#F7AB0A]/50'>{t('AboutPage_text_2')}</span>{" "}
-                    {t('AboutPage_text_3')}
-                </h4>
-                <p className='text-base text-justify'>
-                    {t('AboutPage_text_4')}
-                </p>
-            </motion.div>
-        </motion.div>
-    )
+  const { t } = useTranslation()
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
+    >
+      <Title title={t('About')} />
+      <motion.div
+        initial={{ x: -200, opacity: 0 }}
+        transition={{ duration: 1.2 }}
+        whileInView={{ x: 0, opacity: 1 }}
+        viewport={{ once: true }}
+        className="about-image-container mt-36 md:mt-0"
+      >
+        <Image
+          alt='Loan'
+          src={loan}
+          className="about-image"
+          objectFit='cover'
+          layout='responsive'
+        />
+      </motion.div>
+      <motion.div
+        initial={{ x: 200, opacity: 0 }}
+        transition={{ duration: 1.2 }}
+        whileInView={{ x: 0, opacity: 1 }}
+        viewport={{ once: true }}
+        className='space-y-5 px-0 md:px-10 mt-40 md:mt-0'
+      >
+        <h4 className='text-3xl md:text-4xl font-semibold'>
+          {t('AboutPage_text_1')}{" "}
+          <span className='underline decoration-[#F7AB0A]/50'>{t('AboutPage_text_2')}</span>{" "}
+          {t('AboutPage_text_3')}
+        </h4>
+        <p className='text-base text-justify'>
+          {t('AboutPage_text_4')}
+        </p>
+      </motion.div>
+    </motion.div>
+  )
 }
--- components/Views/Header.tsx
+++ components/Views/Header.tsx
@@ -6,62 +6,60 @@ import { useTranslation } from "react-i18next"
 import React from "react"
 
 export const Header = () => {
-    const { t } = useTranslation()
-    const { i18n } = useTranslation()
-    const icons = [
-        "https://www.linkedin.com/in/loan-cleris-8275b5136/",
-        "https://www.facebook.com/loan.cleris/",
-        "https://www.instagram.com/loan_cleris/",
-        "https://open.spotify.com/user/kaomagni",
-        "https://github.com/TheHikuro"
-    ]
-    const [checked, setChecked] = React.useState(false)
-    const handleLngChange = (lng: string) => {
-        setChecked(!checked)
-        i18n.changeLanguage(lng)
-    }
-    return (
-        <Fragment>
-            <header className="sticky top-0 p-5 flex flex-items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
-                <motion.div
-                    className="flex flex-row items-center"
-                    initial={{ x: -500, opacity: 0, scale: 0.5 }}
-                    animate={{ x: 0, opacity: 1, scale: 1 }}
-                    transition={{ duration: 1.5 }}
-                >
-                    {icons.map((icon, index) => (
-                        <SocialIcon
-                            key={index}
-                            url={icon}
-                            fgColor="gray"
-                            bgColor="transparent"
-                        />
-                    ))}
-                </motion.div>
-                <motion.div
-                    className="flex items-center text-gray-300 cursor-pointer"
-                    initial={{ x: 500, opacity: 0, scale: 0.5 }}
-                    animate={{ x: 0, opacity: 1, scale: 1 }}
-                    transition={{ duration: 1.5 }}
-                >
-                    <Link href="#contact">
-                        <div>
-                            <SocialIcon
-                                network="email"
-                                fgColor="gray"
-                                bgColor="transparent"
-                            />
-                            <span className="uppercase hidden md:inline-flex text-sm text-gray-400">{t('TakeContact')}</span>
-                        </div>
-                    </Link>
-                    <span
-                        onClick={() => handleLngChange(checked ? 'en' : 'fr')}
-                        className="text-2xl md:ml-5 rounded-full hover:border-2 hover:border-gray-300 p-4 cursor-pointer w-7 h-7 object-center flex items-center justify-center overflow-hidden transition-all duration-500 ease-in-out"
-                    >
-                        {!checked ? '🇬🇧' : '🇫🇷'}
-                    </span>
-                </motion.div>
-            </header>
-        </Fragment>
-    )
+  const { t } = useTranslation()
+  const { i18n } = useTranslation()
+  const icons = [
+    "https://www.linkedin.com/in/loan-cleris-8275b5136/",
+    "https://www.facebook.com/loan.cleris/",
+    "https://www.instagram.com/loan_cleris/",
+    "https://open.spotify.com/user/kaomagni",
+    "https://github.com/TheHikuro"
+  ]
+  const [checked, setChecked] = React.useState(false)
+  const handleLngChange = (lng: string) => {
+    setChecked(!checked)
+    i18n.changeLanguage(lng)
+  }
+  return (
+    <Fragment>
+      <header className="sticky top-0 p-5 flex flex-items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
+        <motion.div
+          className="flex flex-row items-center"
+          initial={{ x: -500, opacity: 0, scale: 0.5 }}
+          animate={{ x: 0, opacity: 1, scale: 1 }}
+          transition={{ duration: 1.5 }}
+        >
+          {icons.map((icon, index) => (
+            <SocialIcon
+              key={index}
+              url={icon}
+              fgColor="gray"
+              bgColor="transparent"
+            />
+          ))}
+        </motion.div>
+        <motion.div
+          className="flex items-center text-gray-300 cursor-pointer"
+          initial={{ x: 500, opacity: 0, scale: 0.5 }}
+          animate={{ x: 0, opacity: 1, scale: 1 }}
+          transition={{ duration: 1.5 }}
+        >
+          <Link href="#contact">
+            <SocialIcon
+              network="email"
+              fgColor="gray"
+              bgColor="transparent"
+            />
+            <span className="uppercase hidden md:inline-flex text-sm text-gray-400">{t('TakeContact')}</span>
+          </Link>
+          <span
+            onClick={() => handleLngChange(checked ? 'en' : 'fr')}
+            className="text-2xl md:ml-5 rounded-full hover:border-2 hover:border-gray-300 p-4 cursor-pointer w-7 h-7 object-center flex items-center justify-center overflow-hidden transition-all duration-500 ease-in-out"
+          >
+            {!checked ? '🇬🇧' : '🇫🇷'}
+          </span>
+        </motion.div>
+      </header>
+    </Fragment>
+  )
 }
